285053b58d67ab36d7dd9070c451620726499155
cdavies7845 <cdavies7845@C588MJ64487.hwdsb.on.ca>
2015-11-20 10:33:22 -0500

    added starting code

--- last-stand/src/last/stand/Menu.java
+++ last-stand/src/last/stand/Menu.java
@@ -1,7 +1,28 @@
-
 package last.stand;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-
-public class Menu {
+public class Menu extends BasicGame {
     
+    @Override
+    public void render(GameContainer gc, Graphics g)throws SlickException {
+        
+    }
+    
+    @Override
+    public void update(GameContainer gc, int t)throws SlickException{
+        
+    }
+    
+    @Override
+    public void init(GameContainer gc)throws SlickException{
+        
+    }
+    
+    public Menu () {
+        super("Menu");
+    }
+            
 }
