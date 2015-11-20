fdde211b1b0f0eb6944d37d87054dcca4952f255
cdavies7845 <cdavies7845@C588MJ64487.hwdsb.on.ca>
2015-11-20 11:12:56 -0500

    Fixed screen error

--- last-stand/src/last/stand/Menu.java
+++ last-stand/src/last/stand/Menu.java
@@ -3,26 +3,27 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.*;
 
-public class Menu extends BasicGame {
+public class Menu extends BasicGameState {
     
-    @Override
-    public void render(GameContainer gc, Graphics g)throws SlickException {
-        
+    public Menu (int state) {  
     }
     
-    @Override
-    public void update(GameContainer gc, int t)throws SlickException{
-        
+    public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{   
     }
     
-    @Override
-    public void init(GameContainer gc)throws SlickException{
-        
+    public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException { 
     }
     
-    public Menu () {
-        super("Menu");
+    public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{  
     }
+    public int getID(){
+        return 0;
+    }
+   
+    
+    
+    
             
 }
