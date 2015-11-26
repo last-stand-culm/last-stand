d3a6413828ff688bcc8b5a7fe0f6787275e4cf83
veryslickguy <gamefreak501st@gmail.com>
2015-11-25 19:24:30 -0500

    added a state called instruction this is where all the rules of the game will be

---
+++ last-stand/src/last/stand/Instruction.java
@@ -0,0 +1,26 @@
+package last.stand;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Instruction extends BasicGameState {
+    
+    public Instruction(int state){
+        
+    }
+    
+    public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
+    
+    }
+    
+    public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException {
+         
+     }
+     
+     public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{
+         
+     }
+     
+      public int getID(){
+        return 1;
+    }
+}
