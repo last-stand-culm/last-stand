c178524c7021b2aa681f4b81f3b3ecf466081792
veryslickguy <gamefreak501st@gmail.com>
2015-11-25 19:25:21 -0500

    finished adding the pictures to the menu only thing left is music

--- last-stand/src/last/stand/Menu.java
+++ last-stand/src/last/stand/Menu.java
@@ -4,20 +4,50 @@ import org.newdawn.slick.state.*;
 
 public class Menu extends BasicGameState {
     
+    private float x,y;
     private Image menuPic;
+    private Image playButton;
+    private Image optionButton;
+    private Image playButtonSelect;
+    private Image optionButtonSelect;
     
     public Menu (int state) {  
     }
     
     public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{  
         menuPic=new Image("res/main-menu.Jpg");
+        playButton=new Image("res/play_button.png");  
+        optionButton=new Image("res/option_button.png");  
+        playButtonSelect=new Image("res/play_button_selected.png");  
+        optionButtonSelect=new Image("res/option_button_selected.png");  
     }
     
     public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException { 
         menuPic.draw();
+        playButton.draw(20,150);
+        optionButton.draw(20,350);
+        if(x>=44 && y>=198 && x<=406 && y<=306 ){
+        playButtonSelect.draw(20,150);
+        }
+        if(x>=45 && y>=398 && x<=406 && y<=508){
+        optionButtonSelect.draw(20,350);
+        }
+        
     }
     
     public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{  
+        Input input=gc.getInput();
+        x=input.getMouseX();
+        y=input.getMouseY();
+        
+        if(x>=44 && y>=198 && x<=406 && y<=306 ){
+            if(input.isMouseButtonDown(0)){
+                sbg.enterState(1);
+            }
+        }
+        
+        System.out.println(x);
+        System.out.println(y);
     }
     public int getID(){
         return 0;
