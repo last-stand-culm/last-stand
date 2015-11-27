0a396b4b0a729e66318f9ba4d5d28702559936f3
veryslickguy <gamefreak501st@gmail.com>
2015-11-27 14:38:12 -0500

    worked on instruction menu

--- last-stand/src/last/stand/Instruction.java
+++ last-stand/src/last/stand/Instruction.java
@@ -5,6 +5,8 @@ import org.newdawn.slick.state.*;
 public class Instruction extends BasicGameState {
     
     private Image instructionPic;
+    private Image playButtonInstruction;
+    private Image playButtonSelectInstruction;
     private float x,y;
     
     public Instruction(int state){
@@ -13,10 +15,14 @@ public class Instruction extends BasicGameState {
     
     public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
      instructionPic=new Image("res/instructions.Jpg");
+     playButtonInstruction=new Image ("res/play_button.png");
+     playButtonSelectInstruction=new Image("res/play_button_selected.png");
     }
     
     public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException {
-     instructionPic.draw();    
+     instructionPic.draw();   
+     playButtonInstruction.draw(1,635);
+     if(x>=25 && y>=682 && x<=386 && y<=792){playButtonSelectInstruction.draw(1,635);}
      }
      
      public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{
@@ -24,16 +30,13 @@ public class Instruction extends BasicGameState {
         x=input.getMouseX();
         y=input.getMouseY();
         
-        if(x>=44 && y>=198 && x<=406 && y<=306 ){
+        // if mouse is over the play button and button is clicked
+        if(x>=25 && y>=682 && x<=386 && y<=792){
             if(input.isMouseButtonDown(0)){
-                sbg.enterState(1);
+                
             }
         }
-        if(x>=45 && y>=398 && x<=406 && y<=508){
-          if(input.isMouseButtonDown(0)){
-              sbg.enterState(2);
-          }  
-        }
+        
         
         System.out.println(x);
         System.out.println(y);
