37f7b24eb86ed277a3aa14c3ef0deab202aecd54
veryslickguy <gamefreak501st@gmail.com>
2015-11-27 15:45:41 -0500

    added Game class

---
+++ last-stand/src/last/stand/Game.java
@@ -0,0 +1,28 @@
+package last.stand;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+
+public class Game extends BasicGameState {
+    
+    public Game(int state){
+        
+    }
+    
+    public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
+    
+    }
+    
+    public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException {
+     
+     }
+     
+     public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{
+        
+     }
+     
+      public int getID(){
+        return 2;
+    }
+}
