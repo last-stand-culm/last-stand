4b8c3a5d81f5d44601df33fea3e10b637e543ea9
veryslickguy <gamefreak501st@gmail.com>
2015-11-27 13:03:49 -0500

    finishing option menu

--- last-stand/src/last/stand/Options.java
+++ last-stand/src/last/stand/Options.java
@@ -5,19 +5,54 @@ import org.newdawn.slick.state.*;
 
 
 public class Options extends BasicGameState {
-       
+    
+    private boolean musicOn =true;
+    private boolean easy=true;
     private float x,y;
+    private Image optionPic;
+    private Image exitButton;
+    private Image exitButtonSelected;
+    private Image musicButtonOnGreen;
+    private Image musicButtonOnRed;
+    private Image musicButtonOffGreen;
+    private Image musicButtonOffRed;
+    private Image easyRed;
+    private Image easyGreen;
+    private Image hardRed;
+    private Image hardGreen;
+    private Image scoreButton;
+    private Image scoreButtonSelect;
     
     public Options(int state){
         
     }
     
     public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
-    
+        optionPic=new Image("res/option.Jpg");
+        exitButton=new Image("res/exit_button.png");
+        exitButtonSelected=new Image("res/exit_button_select.png");
+        musicButtonOnGreen=new Image("res/music_button_on_green.png");
+        musicButtonOnRed=new Image("res/music_button_on_red.png");
+        musicButtonOffGreen=new Image("res/music_off_green.png");
+        musicButtonOffRed=new Image("res/music_off_red.png");
+        easyRed=new Image("res/difficullty_easy_red.png");
+        easyGreen=new Image("res/difficullty_easy_green.png");
+        hardRed=new Image("res/difficullty_hard_red.png");
+        hardGreen=new Image("res/difficullty_hard_green.png");
+        scoreButton=new Image("res/score_button.png");
+        scoreButtonSelect=new Image("res/score_button_selected.png");
     }
     
     public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException {
-         
+        optionPic.draw();
+        exitButton.draw(5,550);
+        if(musicOn==true){musicButtonOnGreen.draw(250,55);}
+        if(musicOn==true){musicButtonOffRed.draw(700,55);}
+        if(musicOn==false){musicButtonOnRed.draw(250,55);}
+        if(musicOn==false){musicButtonOffGreen.draw(700,55);}
+        if(easy==true){easyGreen.draw(330,200);}
+        if(easy==true){hardRed.draw(700,200);}
+        scoreButton.draw(500,400);
      }
      
      public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{
