fd69c5c1dddd9e403e353d8c5571a313fb9ffdab
chees <chees@DESKTOP-CAFLVNI>
2016-01-23 17:15:17 -0500

    Scores does not work. Something is wrong with it, and I'm not 100% sure how the states work, so chances are, I missed something. The file reader writer does work now though.

---
+++ last-stand/src/last/stand/Scores.java
@@ -0,0 +1,21 @@
+package last.stand;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+import org.lwjgl.input.Mouse;
+import java.io.*;
+public class Scores extends BasicGameState{
+    private Image background;
+    private int time;
+    public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
+        background=new Image("res/score_screen.png");
+    }
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException{
+        g.drawString("Tester                                        Tester",100,100);
+    }
+    public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException{
+        
+    }
+    public int getID(){
+        return 5;
+    }
+}
