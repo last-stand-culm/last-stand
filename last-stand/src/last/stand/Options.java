121235ec9616116b09ac4c0552db1a96444828aa
veryslickguy <gamefreak501st@gmail.com>
2016-01-24 21:33:19 -0500

    adding comments

--- last-stand/src/last/stand/Options.java
+++ last-stand/src/last/stand/Options.java
@@ -30,7 +30,7 @@ public class Options extends BasicGameState {
     }
     
     public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
-        //finding the location of all the images and placing them with their variables
+//finding the location of all the images and placing them with their variables
         optionPic=new Image("res/option.Jpg");
         exitButton=new Image("res/exit_button.png");
         exitButtonSelected=new Image("res/exit_button_select.png");
@@ -44,23 +44,32 @@ public class Options extends BasicGameState {
         hardGreen=new Image("res/difficullty_hard_green.png");
         scoreButton=new Image("res/score_button.png");
         scoreButtonSelect=new Image("res/score_button_selected.png");
+//------------------------------------------------------------------------------
     }
     
     public void render(GameContainer gc,StateBasedGame sbg, Graphics g)throws SlickException {
-        //rendering all the images and strings
+//rendering all the images and strings
         optionPic.draw();
         exitButton.draw(5,550);
+//music button turn on/off        
         if(musicOn==true){musicButtonOnGreen.draw(250,55);}
         if(musicOn==true){musicButtonOffRed.draw(700,55);}
         if(musicOn==false){musicButtonOnRed.draw(250,55);}
         if(musicOn==false){musicButtonOffGreen.draw(700,55);}
+//------------------------------------------------------------------------------
+//easy button turn on/off
         if(easy==true){easyGreen.draw(330,200);}
         if(easy==true){hardRed.draw(700,200);}
         if(easy==false){easyRed.draw(330,200);}
         if(easy==false){hardGreen.draw(700,200);}
-        scoreButton.draw(500,400);
+//------------------------------------------------------------------------------
+        
+        scoreButton.draw(500,400);//rendering score button
+        
+//if mouse hovers over either buttons it highlights them        
         if(x>=33 && y>=601 && x<=390 && y<=705){exitButtonSelected.draw(5,550);}
         if(x>=527 && y>=449 && x<=883 && y<=555){scoreButtonSelect.draw(500,400);}
+//--------------------------------------------------------------------------------        
      }
      
      public void update(GameContainer gc,StateBasedGame sbg, int delta)throws SlickException{
@@ -68,39 +77,46 @@ public class Options extends BasicGameState {
         x=input.getMouseX();
         y=input.getMouseY();
        
-        //when mouse is over button off and button is clicked
+//when mouse is over button off and button is clicked
        if(x>=726 && y>=104 && x<=1083 && y<=209){
             if(input.isMouseButtonDown(0) && musicOn==true){
                 musicOn=false;
             }
        } 
-       //when mouse is over button on and buton is clicked
+//------------------------------------------------------------------------------       
+//when mouse is over button on and buton is clicked
        if(x>=275 && y>=103 && x<=633 && y<=210){
           if(input.isMouseButtonDown(0) && musicOn==false){
               musicOn=true;
           } 
        }
-       //when mouse is over button easy and button is clicked
+//------------------------------------------------------------------------------
+//when mouse is over button easy and button is clicked
        if(x>=356 && y>=249 && x<=714 && y<=354){
          if(input.isMouseButtonDown(0) && easy==false){
              easy=true;
          }  
-       } 
-       //when mouse is over button hard and button is clicked
+       }
+//------------------------------------------------------------------------------
+//when mouse is over button hard and button is clicked
        if(x>=726 && y>=248 && x<=1083 && y<=354){
            if(input.isMouseButtonDown(0) && easy==true){
                easy=false;
            }
        }
-       //when mouse is over exit button which brings you back to menu
+//------------------------------------------------------------------------------
+//when mouse is over exit button which brings you back to menu
        if(x>=33 && y>=601 && x<=390 && y<=705){
           if(input.isMouseButtonDown(0)){
                sbg.enterState(0);
            }
       }
+//------------------------------------------------------------------------------
+//if mouse is pushed enter in score screen
         if(x>=527 && y>=449 && x<=883 && y<=555 && input.isMouseButtonDown(0)){
             sbg.enterState(5);
         }
+//------------------------------------------------------------------------------        
         
      }
      
